268572bb38911165f66fdb24d80ea8892b8ebfcf
Kyle <kazinsser@gmail.com>
2017-08-16 00:30:35 +0000

    Initial commit.

---
+++ r6stats.py
@@ -0,0 +1,22 @@
+import asyncio
+import r6sapi as api
+
+
+@asyncio.coroutine
+def run():
+    auth = api.Auth("username", "password")
+
+    player = yield from auth.get_player("Kazology", api.Platforms.UPLAY)
+    operator = yield from player.get_operator("hibana")
+    print("hibana kills: ")
+    print(operator.kills)
+    operator = yield from player.get_operator("sledge")
+    print("sledge kills: ")
+    print(operator.kills)
+    operator = yield from player.get_operator("ash")
+    print("ash kills: ")
+    print(operator.kills)
+    yield from auth.session.close()
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(run())
